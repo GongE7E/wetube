d3c0b9c1749e7e44bc954e26a08f8fb5989aeeb9
HanGyeong <gong4737@icloud.com>
2022-12-16 22:38:14 +0900

    logged in user

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 
 import express  from "express";
 import morgan from "morgan";
+import session from "express-session";
 import routeRouter from "./routers/routeRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 
 const app = express();
@@ -12,6 +14,13 @@ app.set("view engine","pug");
 app.set("views",process.cwd() +"/src/views");
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret:"Hello",
+    resave:true,
+    saveUninitialized:true,
+})
+);
+app.use(localsMiddleware);
 app.use("/",routeRouter);
 app.use("/users",userRouter);
 app.use("/videos",videoRouter);
