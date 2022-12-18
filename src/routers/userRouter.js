d59c67abf4e678d157eaa8853f847c5120b3d35d
HanGyeong <gong4737@icloud.com>
2022-12-18 20:10:19 +0900

    github login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from"express";
-import {editUser,remove,logout,see} from"../controllers/userController";
+import {editUser,remove,logout,see,startGithubLogin,finishGithubLogin} from"../controllers/userController";
 
 
 const userRouter=express.Router();
@@ -8,7 +8,8 @@ userRouter.get("/logout",logout);
 userRouter.get("/edit",editUser);
 userRouter.get("/remove",remove);
 userRouter.get("/:id(\\d+)",see);
-
+userRouter.get("/github/start",startGithubLogin);
+userRouter.get("/github/finish",finishGithubLogin);
 
 export default "userRouter";
 
