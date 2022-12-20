551bb8b39e480197969a2b70415b78b88af14291
HanGyeong <gong4737@icloud.com>
2022-12-20 19:20:14 +0900

    portector and Public Middleware

--- src/routers/routeRouter.js
+++ src/routers/routeRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {getJoin,postJoin,getLogin,postLogin} from "../controllers/userController";
 import {home,search} from "../controllers/videoController";
-
+import {publicOnlyMiddleware} from "../middlewares";
 
 const routeRouter=express.Router();
 
 
 routeRouter.get("/",home);
-routeRouter.route("/join").get(getJoin).post(postJoin);
-routeRouter.route("/login").get(getLogin).post(postLogin);
+routeRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+routeRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 routeRouter.get("/search",search);
 
 export default routeRouter;
